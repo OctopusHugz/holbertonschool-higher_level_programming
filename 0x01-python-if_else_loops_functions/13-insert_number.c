ed4ce6dc68f8cf009b1ec04b6dc4820617b98501
OctopusHugz <colsonscott53@gmail.com>
2020-04-29 02:25:00 +0000

    Commented out head null check to see it's effect on checker output

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,8 +15,8 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node, *temp;
 
-	if (head == NULL)
-		return (NULL);
+	/* if (head == NULL)
+		return (NULL); */
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
 		return (NULL);
