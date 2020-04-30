7cca465470cea9ec501eaf737870a0f6e6efbdeb
OctopusHugz <colsonscott53@gmail.com>
2020-04-30 17:02:42 +0000

    Added solution for exercise 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+pass
+if __name__ == "__main__":
+    length = len(argv)
+    if length == 1:
+        print("0")
+    if length == 2:
+        print(argv[1])
+    if length > 2:
+        i = 1
+        result = 0
+        while i < length:
+            num = int(argv[i])
+            result += num
+            i += 1
+        print(result)
