5a6bed2ec558571106f8956fc7cd8c14695dbd13
OctopusHugz <colsonscott53@gmail.com>
2020-05-05 17:24:31 +0000

    Added logic to handle edge cases

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if idx < 0 or len(my_list) < idx:
+    if idx < 0 or len(my_list) - 1 < idx:
         return new_list
     for i in my_list:
+        if idx == 0:
+            new_list[0] = element
         if i == idx:
             new_list[i] = element
     return new_list
