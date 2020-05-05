0ab4d5b9b7462f62b27689c629185df1c7712642
OctopusHugz <colsonscott53@gmail.com>
2020-05-05 17:19:21 +0000

    Added logic to handle edge cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or len(my_list) < idx:
+    if idx < 0 or len(my_list) - 1 < idx:
         return my_list
     for i in my_list:
+        if idx == 0:
+            my_list[0] = element
+            return my_list
         if i == idx:
             my_list[i] = element
             return my_list
