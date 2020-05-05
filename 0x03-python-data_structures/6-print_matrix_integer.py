508b606848a9ec3dd86c7767595439d961e39220
OctopusHugz <colsonscott53@gmail.com>
2020-05-05 17:57:51 +0000

    Added null check on matrix[0]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    num_lists = len(matrix)
-    for i in range(num_lists):
-        list_items = len(matrix[i])
-        for j in range(list_items):
-            if j != list_items - 1:
-                print("{:d} ".format(matrix[i][j]), end='')
-            else:
-                print("{:d}".format(matrix[i][j]))
-    if num_lists == 1:
+    k = 0
+    if (matrix[k]):
+        num_lists = len(matrix)
+        for i in range(num_lists):
+            list_items = len(matrix[i])
+            for j in range(list_items):
+                if j != list_items - 1:
+                    print("{:d} ".format(matrix[i][j]), end='')
+                else:
+                    print("{:d}".format(matrix[i][j]))
+    else:
         print("")
