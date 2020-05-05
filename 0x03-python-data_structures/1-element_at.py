9916e8ca62d8c8d04185752a571723706cea53e3
OctopusHugz <colsonscott53@gmail.com>
2020-05-05 17:14:35 +0000

    Added logic to handle idx == 0

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,7 @@ def element_at(my_list, idx):
     if idx < 0 or len(my_list) < idx:
         return None
     for i in my_list:
+        if idx == 0:
+            return my_list[0]
         if i == idx:
-            return(my_list[i])
+            return my_list[i]
