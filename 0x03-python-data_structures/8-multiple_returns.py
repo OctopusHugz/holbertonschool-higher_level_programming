28e850d82f68073894d2db4aa5a3f61f152df81f
OctopusHugz <colsonscott53@gmail.com>
2020-05-05 18:56:36 +0000

    Added logic to handle empty sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[:1]
+    if (length != 0):
+        first = sentence[:1]
+    else:
+        first = None
     return length, first
