05b2b47f5a1d333d3c97a9030a05fe15208c1b64
OctopusHugz <colsonscott53@gmail.com>
2020-05-06 04:05:38 +0000

    Added first attempt at solution for exercise 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -10,13 +11,26 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *node;
+	int array[1024], i = 0, half = 0, increment = 1, decrement = 0;
 
 	if (*head == NULL)
 		return (1);
 	node = *head;
 	while (node)
 	{
+		array[i] = node->n;
 		node = node->next;
+		i++;
 	}
-	return (0);
+	half = i / 2 - 1;
+	printf("half is: %d\n", half);
+	while (i > half + 1)
+	{
+		if (array[half - decrement] == array[half + increment])
+			printf("Repeat found at %d\n", array[half - decrement]);
+		else
+			return (0);
+		increment++, decrement++, i--;
+	}
+	return (1);
 }
