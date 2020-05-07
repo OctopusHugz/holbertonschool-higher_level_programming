e7e5058f7a2942ee33b153ee1c027f4faa7ede9c
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 18:45:27 +0000

    Added much shorter refactor after using set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_list = []
-    result = 0
-    for i in my_list:
-        if i not in uniq_list:
-            result += i
-            uniq_list.append(i)
+    result = sum(set(my_list))
     return result
