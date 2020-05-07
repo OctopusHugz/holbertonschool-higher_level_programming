d52f0d9972d9aa5413fcea08a243154f870fe162
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 21:55:09 +0000

    Added solution for exercise 12. Doesn't handle IX or any case where subtracting is required

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    result = 0
     if type(roman_string) != str and type(roman_string) is not None:
         return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for char in roman_string:
+        for key, value in roman_dict.items():
+            if char == key:
+                result += int(value)
+    return result
