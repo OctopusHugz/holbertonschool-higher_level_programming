7c89f39b0e1863a8a5a69f7a6c535da36bea6b5f
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 17:29:21 +0000

    Refactored back to simpler for statement

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    if matrix:
-        for lists in range(len(matrix)):
-            new_matrix.append(list(map(lambda x: x * x, matrix[lists])))
+    for lists in matrix:
+        new_matrix.append(list(map(lambda x: x * x, lists)))
     return new_matrix
