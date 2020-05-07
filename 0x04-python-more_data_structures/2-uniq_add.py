d7466e4c24844cb37447aefdfffafa884daafa6b
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 18:36:46 +0000

    Added solution for exercise 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = []
+    result = 0
+    if my_list:
+        for i in my_list:
+            if i not in uniq_list:
+                result += i
+                uniq_list.append(i)
+    return result
