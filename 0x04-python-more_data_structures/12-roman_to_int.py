02238682da9ddcf73e88c53bc7a54efa9ef93382
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 23:20:22 +0000

    Switched back to != str type check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     flag = 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) is not str and type(roman_string) is not None:
+    if type(roman_string) != str and type(roman_string) is not None:
         return 0
     for char in roman_string:
         for key, value in roman_dict.items():
