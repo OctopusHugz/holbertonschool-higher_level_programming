560373049373bc2b9608d517546a75ae85fbd3ae
OctopusHugz <colsonscott53@gmail.com>
2020-05-07 22:48:21 +0000

    Removed comments and changed != str type check to is not str type check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     last = 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) != str and type(roman_string) is not None:
+    if type(roman_string) is not str and type(roman_string) is not None:
         return 0
     for char in roman_string:
         for key, value in roman_dict.items():
@@ -25,18 +25,4 @@ def roman_to_int(roman_string):
             else:
                 result -= values
             count += 1
-        """ result += int(value)
-print(new_list)
-print(new_list[-1:])
-print(new_list[:1])
-if new_list[-1:] > new_list[:1]:
-    print("Need to subtract!")
-    print("Result is {}".format(result))
-    for nums in reversed(new_list):
-        print("Nums is {}".format(nums))
-        if count != 0:
-            diff += nums
-            print("Diff is {}".format(diff))
-        result -= diff
-        count += 1 """
     return result
