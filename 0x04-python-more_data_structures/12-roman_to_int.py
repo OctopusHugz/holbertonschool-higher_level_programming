468651480ad20f06aa45b56c349447c03b638609
OctopusHugz <colsonscott53@gmail.com>
2020-05-08 00:56:35 +0000

    Removed extraneous print statements to test solution

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    new_list = []
     result = 0
     count = 0
     last_value = 0
@@ -11,17 +10,13 @@ def roman_to_int(roman_string):
     for char in roman_string:
         for key, value in roman_dict.items():
             if char == key:
-                print("Adding value of {} to result".format(value))
                 result += value
                 if count == 0:
                     last_value = value
                     count += 1
                     continue
-                print("Last value is {}".format(last_value))
                 if last_value < value:
-                    print("Subtracting value of {} to result".format(last_value))
                     result -= last_value
-                    print("Subtracting value of {} to result".format(last_value))
                     result -= last_value
                 count += 1
                 last_value = value
