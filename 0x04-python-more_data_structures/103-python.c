fec64a8f2439f2899c7560102f24544079e02422
OctopusHugz <colsonscott53@gmail.com>
2020-05-08 05:46:53 +0000

    Added updated outline for exercise 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -13,13 +13,18 @@ void print_python_list(PyObject *p)
 {
 	int len = PyList_Size(p), count = 0;
 	PyListObject *object = (PyListObject *)p;
+	char const *type = "";
 
-	printf("[*] Python List Info\n");
-	printf("[*] Size of the Python List = %d\n")
-	printf("[*] Allocated = %lu\n", len, object->allocated);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %lu\n", object->allocated);
 	while (count < len)
 	{
-		printf("Element %d: %s\n", count, Py_TYPE(object->ob_item[count])->tp_name);
+		type = Py_TYPE(object->ob_item[count])->tp_name;
+		printf("Element %d: %s\n", count, type);
+		if (strcmp(type, "bytes") == 0)
+			printf("Found bytes!\n");
+			/* print_python_bytes(p); */
 		count++;
 	}
 }
@@ -31,6 +36,14 @@ void print_python_list(PyObject *p)
 
 void print_python_bytes(PyObject *p)
 {
-	if (p)
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	int size = PyBytes_Size(p), len = PyList_GET_SIZE(p), count = 0;
+
+	if (count < len)
+	{
 		printf("[.] bytes object info\n");
+		printf("  size: %d\n  trying string: %s\n", size, bytes->ob_sval);
+		printf("  first 6 bytes: %s\n", (char *)bytes);
+		count++;
+	}
 }
