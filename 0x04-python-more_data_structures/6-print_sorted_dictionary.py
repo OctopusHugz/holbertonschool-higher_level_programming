cf2bb7cfa84d70b275330f9645805f47f5709220
OctopusHugz <colsonscott53@gmail.com>
2020-05-08 04:12:58 +0000

    Removed check for if dictionary exist to see effect on exercise 9 output checks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary:
-        for key, value in sorted(a_dictionary.items()):
-            print("{}: {}".format(key, value))
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
