bcd0031960867dd5ae77dd1d5fe60da5f22f13c0
OctopusHugz <colsonscott53@gmail.com>
2020-05-08 00:55:25 +0000

    Added new solution with print statements before removing in case need to debug

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,7 @@ def roman_to_int(roman_string):
     new_list = []
     result = 0
     count = 0
-    last = 0
-    flag = 0
+    last_value = 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) != str and type(roman_string) is not None:
@@ -12,20 +11,18 @@ def roman_to_int(roman_string):
     for char in roman_string:
         for key, value in roman_dict.items():
             if char == key:
-                new_list.append(value)
-    first = new_list[0]
-    if len(new_list) > 1:
-        last = new_list[-1]
-    for values in new_list:
-        if first >= last:
-            result += values
-    if result == 0:
-        for values in reversed(new_list):
-            if count == 0 or num == values and flag != 1:
-                result += values
-            else:
-                result -= values
-                flag = 1
-            num = values
-            count += 1
+                print("Adding value of {} to result".format(value))
+                result += value
+                if count == 0:
+                    last_value = value
+                    count += 1
+                    continue
+                print("Last value is {}".format(last_value))
+                if last_value < value:
+                    print("Subtracting value of {} to result".format(last_value))
+                    result -= last_value
+                    print("Subtracting value of {} to result".format(last_value))
+                    result -= last_value
+                count += 1
+                last_value = value
     return result
