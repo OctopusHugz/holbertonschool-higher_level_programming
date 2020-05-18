1b37f7835977b27e23a70b937c68922fcdc624ba
OctopusHugz <colsonscott53@gmail.com>
2020-05-18 17:30:15 +0000

    Added solution for exercise 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as ve:
+        stderr.write("Exception: {}\n".format(ve))
+        return False
+    return True
