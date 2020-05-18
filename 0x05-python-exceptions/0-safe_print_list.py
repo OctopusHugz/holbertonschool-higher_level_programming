e7f164ce192f57cc5985b11e1acbef770fa06bde
OctopusHugz <colsonscott53@gmail.com>
2020-05-18 16:24:41 +0000

    Refactored logic

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[length]), end='')
             count += 1
-            length += 1
         except:
-            length += 1
+            pass
+        length += 1
     print()
     return count
