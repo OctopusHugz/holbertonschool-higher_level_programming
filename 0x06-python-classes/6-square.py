c8bae8bef3a45f4d6cca6d6d1aca26d5e533f987
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 21:23:42 +0000

    Small refactor of size logic

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,11 +29,12 @@ and returns it"""
         """This setter function validates the size argument given at instantiation.
 If valid, it sets the size attribute for the instance."""
         if isinstance(value, int):
-            self.__size = value
+            if value > 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
 
     @property
     def position(self):
