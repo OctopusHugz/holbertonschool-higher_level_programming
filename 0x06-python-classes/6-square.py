30168c48fbc700218a19fa7afbf80980e0368bb1
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 20:55:46 +0000

    Removed comment lines

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,6 @@ class Square():
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
-        # print(position)
-        # print(type(position))
 
     @property
     def size(self):
