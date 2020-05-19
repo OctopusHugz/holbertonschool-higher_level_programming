8bd49435f686cceacf3b2854cb64a2a58974b561
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 04:43:33 +0000

    Added outline for exercise 10

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+
+void print_python_bytes(PyObject *p);
+
+void print_python_float(PyObject *p);
+
+void print_python_list(PyObject *p)
+{
+	(void)p;
+}
+
+void print_python_bytes(PyObject *p)
+{
+	(void)p;
+}
+
+void print_python_float(PyObject *p)
+{
+	(void)p;
+}
