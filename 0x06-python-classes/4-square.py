8292db3957ea7053fd45277df5ee6ecb8809cd3c
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 16:25:16 +0000

    Added docstrings

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,17 +10,25 @@ after verifying it in the setter function. It then uses the getter function
 to obtain that size attribute. It then uses that size to compute its area"""
 
     def __init__(self, size=0):
+        """This function initializes an instance of the Square class and assigns
+the private attribute size to the instance if size is the correct int type."""
         self.__size = size
 
     def area(self):
+        """This function takes the size attribute of the object and uses
+it to compute its area"""
         return self.__size ** 2
 
     @property
     def size(self):
+        """This getter function gets the size attribute of the instance
+and returns it"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """This setter function validates the size argument given at instantiation.
+If valid, it sets the size attribute for the instance."""
         if isinstance(value, int):
             self.__size = value
         else:
