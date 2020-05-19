39e36ffa172ea99801792d6a18791515116dfdef
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 16:00:31 +0000

    Added docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This module declares a Square class that
+initializes a private size attribute"""
+
+
 class Square():
+    """This Square class initializes the instance's private size attribute"""
+
     def __init__(self, size=0):
         self.__size = size
