42c80a6697630191c65a72533a13504e0031d655
OctopusHugz <colsonscott53@gmail.com>
2020-05-19 16:17:26 +0000

    Added __init__ docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,4 +7,6 @@ class Square:
     """This Square class initializes the instance's private size attribute"""
 
     def __init__(self, size=0):
+        """This function initializes an instance of the Square class and assigns
+the private attribute size to the instance."""
         self.__size = size
