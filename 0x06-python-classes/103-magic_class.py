8205ad0cf6a7105f7d50a2da13ef0dcb70e3917e
OctopusHugz <colsonscott53@gmail.com>
2020-05-21 04:22:15 +0000

    Added 0 back to radius in parameter for __init__ and added import math to top of file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """This module declares a MagicClass class that initializes a private
 radius attribute if the radius argument is verified. It then computes
-the area or circumference."""
+the area or circumference. """
+import math
 
 
 class MagicClass:
     """This MagicClass class initializes the instance's private radius attribute
 after verifying it. It also computers area or circumference based on radius."""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         """This function initializes an instance of the MagicClass class and assigns
 the private attribute radius to the instance if radius is the correct int or
