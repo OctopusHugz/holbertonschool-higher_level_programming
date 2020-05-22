db810f77f6e1be9cd7058ac14de979ef62d6e649
OctopusHugz <colsonscott53@gmail.com>
2020-05-22 05:06:40 +0000

    Added docstring to outline

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module implements the say_my_name function"""
+
+
 def say_my_name(first_name, last_name=""):
+    """The say_my_name function verifies that parameters provided to
+the function are strings. Then prints a statement using those names."""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
