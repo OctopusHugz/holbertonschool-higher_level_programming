eb0905065c4ff1ae2b504d1e52d89695f90cffb8
OctopusHugz <colsonscott53@gmail.com>
2020-05-22 16:29:54 +0000

    Added docstrings

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module implements the print_square function"""
+
+
 def print_square(size):
+    """The print_square function verifies that parameter provided to
+the function is an integer. Then prints a square using that integer as size."""
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
