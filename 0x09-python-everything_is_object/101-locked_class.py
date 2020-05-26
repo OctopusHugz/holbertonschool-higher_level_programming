ecb1364ffae8fd4618454650f3b8086dcae8e180
OctopusHugz <colsonscott53@gmail.com>
2020-05-26 23:53:47 +0000

    Added docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""This module creates a LockedClass that only allows a first_name attribute"""
+
+
 class LockedClass:
     __slots__ = ["first_name"]
 
     def __init__(self):
+        """This function creates the LockedClass instance"""
         self.first_name = ""
