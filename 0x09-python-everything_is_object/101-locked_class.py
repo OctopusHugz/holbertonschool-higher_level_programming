8ab36c74b30e2cb4ba913f9f35c650c01d862668
OctopusHugz <colsonscott53@gmail.com>
2020-05-26 23:55:42 +0000

    Added more docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This module creates a LockedClass that only allows a first_name attribute"""
+"""This module creates a LockedClass instance that only allows a first_name
+attribute"""
 
 
 class LockedClass:
+    """This class creates an instance of LockedClass"""
     __slots__ = ["first_name"]
 
     def __init__(self):
