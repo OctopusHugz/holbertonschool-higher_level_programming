f1acbb9cd0e189345be56f46957c523fad2dc086
OctopusHugz <colsonscott53@gmail.com>
2020-06-02 21:38:39 +0000

    Added docstrings to 5 and added solution for 6

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""This module implements the BaseGeometry class"""
+
+
 class BaseGeometry:
+    """This creates an instance of the BaseGeometry class"""
     pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""This module implements the BaseGeometry class"""
+
+
 class BaseGeometry:
-    pass
+    """This creates an instance of the BaseGeometry class"""
+
+    def area(self):
+        """This function raises an Exception"""
+        raise Exception("area() is not implemented")
