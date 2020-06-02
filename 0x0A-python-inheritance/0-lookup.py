db408292891aed78fbf7f8ead3828e2512c7299b
OctopusHugz <colsonscott53@gmail.com>
2020-06-02 14:41:14 +0000

    Added solution for exercise 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module implements the lookup function"""
+
+
+def lookup(obj):
+    """This function looks up the attributes and methods of a given object"""
+    return dir(obj)
