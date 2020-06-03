677bd632fe93fe25ea18dcb203fad4e7a7472eab
OctopusHugz <colsonscott53@gmail.com>
2020-06-03 03:56:27 +0000

    Combined logic into one and statement and fixed indent

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,6 @@ def inherits_from(obj, a_class):
     """This function tests whether a given object is an instance of
 a class that inherited from the given class"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-            return True
+        return True
     else:
         return False
