d14420cbba9c70b07458d798db060badb0066570
OctopusHugz <colsonscott53@gmail.com>
2020-06-03 21:48:15 +0000

    Added solution for exercise 8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module implements the save_to_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function loads a JSON object from a file filename"""
+    with open(filename) as fp:
+        text = fp.read()
+    return json.loads(text)
