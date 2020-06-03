9d5f6e60c4847cb19bb3b6cb94bb2793f549382d
OctopusHugz <colsonscott53@gmail.com>
2020-06-03 03:56:02 +0000

    Combined logic into one and statement

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,7 @@
 def inherits_from(obj, a_class):
     """This function tests whether a given object is an instance of
 a class that inherited from the given class"""
-    if issubclass(type(obj), a_class):
-        if type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
             return True
     else:
         return False
