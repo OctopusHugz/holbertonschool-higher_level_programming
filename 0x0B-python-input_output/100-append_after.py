8803837c4cd6b69d439ddc0d21227b57501cb1fe
OctopusHugz <colsonscott53@gmail.com>
2020-06-04 04:56:29 +0000

    Added rough outline for exercise 15

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,5 +4,13 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """This function appends the new_string after the search_string
-is found in the file filename"""
-    pass
+is found in the file filename """
+    line_num = 0
+    with open(filename, "r+") as fp:
+        while True:
+            line = fp.readlines()
+            line_num += 1
+            if not line:
+                break
+            if search_string in line:
+                fp.insert(line_num + 1, new_string)
