8bce8dccf93c61a127ada0972fd6a13e20f65749
OctopusHugz <colsonscott53@gmail.com>
2020-06-04 04:40:22 +0000

    Added outline for exercise 15

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module implements the append_after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function appends the new_string after the search_string
+is found in the file filename"""
+    pass
