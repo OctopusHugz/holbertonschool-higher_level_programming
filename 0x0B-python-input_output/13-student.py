c6fcc8119ec4b84d63641e06d59d901517868fb8
OctopusHugz <colsonscott53@gmail.com>
2020-06-04 04:07:56 +0000

    Added solution for exercise 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module implements the Student class"""
+
+
+class Student:
+    """This is an instance of the Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """This function creates the instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This function returns a dictionary representation of the instance"""
+        strings_list = []
+        new_dict = {}
+        if attrs and type(attrs) == list:
+            for strings in attrs:
+                if type(strings) != str:
+                    return self.__dict__
+                if strings in self.__dict__.keys():
+                    strings_list.append(strings)
+            new_dict = new_dict.fromkeys(strings_list)
+            for keys in new_dict.keys():
+                value = getattr(self, keys)
+                new_dict[keys] = value
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """This function replaces all attributes of the instance"""
+        for key, value in json.items():
+            self.__dict__[key] = value
