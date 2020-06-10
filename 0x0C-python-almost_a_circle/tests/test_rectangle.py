3cde0d51eedcc4f19aea130920da30c3fea9a203
OctopusHugz <colsonscott53@gmail.com>
2020-06-10 03:22:35 +0000

    Added tests for update methods for Square and Rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -12,17 +12,43 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
+
         r2 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r2.x, 0)
         self.assertEqual(r2.y, 0)
         self.assertEqual(r2.id, 12)
+
         r3 = Rectangle(10, 2, 42, 98, 12)
         self.assertEqual(r3.x, 42)
         self.assertEqual(r3.y, 98)
         self.assertEqual(r3.id, 12)
         self.assertEqual(r3.__str__(), "[Rectangle] (12) 42/98 - 10/2")
+
         r4 = Rectangle(10, 2, 42, 98, -12)
         self.assertEqual(r4.id, -12)
+        self.assertEqual(r4.area(), 20)
+
+        r5 = Rectangle(1, 1, 1, 1, 1)
+        r5.update(13, 22, 42, 98, 140)
+        self.assertEqual(r5.id, 13)
+        self.assertEqual(r5.width, 22)
+        self.assertEqual(r5.height, 42)
+        self.assertEqual(r5.x, 98)
+        self.assertEqual(r5.y, 140)
+
+        r6 = Rectangle(1, 1, 1, 1, 1)
+        r6.update(x=98, height=42, y=140, id=13, width=22)
+        self.assertEqual(r6.id, 13)
+        self.assertEqual(r6.width, 22)
+        self.assertEqual(r6.height, 42)
+        self.assertEqual(r6.x, 98)
+        self.assertEqual(r6.y, 140)
+
+        # r5 = Rectangle(2, 2)
+        # self.assertEqual(r5.display(), "#""#""\n""#""#")
+
+        # r6 = Rectangle(2, 2, 2, 2)
+        # self.assertEqual(r6.display(), "\n""\n"" "" ""#""#""\n"" "" ""#""#")
 
         with self.assertRaises(TypeError):
             r1 = Rectangle()
@@ -35,23 +61,13 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             r1 = Rectangle("holbie")
         with self.assertRaises(TypeError):
-            r1 = Rectangle([42])
-        with self.assertRaises(TypeError):
             r1 = Rectangle("holbie", 98)
-        with self.assertRaises(TypeError):
-            r1 = Rectangle([42], 98)
         with self.assertRaises(TypeError):
             r1 = Rectangle(42, "holbie")
         with self.assertRaises(TypeError):
-            r1 = Rectangle(42, [98])
-        with self.assertRaises(TypeError):
             r1 = Rectangle(42, 98, "holbie", 0)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(42, 98, [13], 0)
-        with self.assertRaises(TypeError):
             r1 = Rectangle(42, 98, 0, "holbie")
-        with self.assertRaises(TypeError):
-            r1 = Rectangle(42, 98, 0, [22])
 
         with self.assertRaises(ValueError):
             r1 = Rectangle(-42, 98)
--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -11,13 +11,30 @@ class TestSquare(unittest.TestCase):
         """This function tests the setting of id attribute"""
         s1 = Square(42)
         self.assertEqual(s1.size, 42)
+
         s2 = Square(42, 13, 22, 98)
         self.assertEqual(s2.x, 13)
         self.assertEqual(s2.y, 22)
         self.assertEqual(s2.id, 98)
         self.assertEqual(s2.__str__(), "[Square] (98) 13/22 - 42")
+
         s3 = Square(42, 13, 22, -98)
         self.assertEqual(s3.id, -98)
+        self.assertEqual(s3.area(), 1764)
+
+        s4 = Square(1, 1, 1, 1)
+        s4.update(13, 22, 42, 98)
+        self.assertEqual(s4.id, 13)
+        self.assertEqual(s4.size, 22)
+        self.assertEqual(s4.x, 42)
+        self.assertEqual(s4.y, 98)
+
+        s5 = Square(1, 1, 1, 1)
+        s5.update(y=98, size=22, id=13,  x=42)
+        self.assertEqual(s5.id, 13)
+        self.assertEqual(s5.size, 22)
+        self.assertEqual(s5.x, 42)
+        self.assertEqual(s5.y, 98)
 
         with self.assertRaises(TypeError):
             s1 = Square()
