18af1b004d68e7ec2bd79bb47c985a76b0e0e385
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 18:09:10 +0000

    Added solution for exercise 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script inserts a new row in the table first_table
+-- This query inserts a new row in the table first_tablee
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
