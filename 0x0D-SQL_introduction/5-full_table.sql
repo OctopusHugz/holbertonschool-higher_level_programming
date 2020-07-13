e7c0b00f1c5fe09e8ef0f97f3e83efa6de2e45c0
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 17:57:57 +0000

    Updated solution for exercise 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- This script prints the full description of the table first_table
 -- This query prints the full description of the table first_table
-SHOW COLUMNS FROM first_table;
+SHOW CREATE TABLE first_table;
