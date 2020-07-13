d853da7a81e4c14b740e94cb5d98372e1c309dee
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 18:36:00 +0000

    Added solution for exercise 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records of the table second_table by highest score
+-- This query lists all records of the table second_table by highest score
+SELECT score, name FROM second_table ORDER BY score DESC
