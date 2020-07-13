affd8c371847667c77a85d17ecf6bd20c4b7ddc7
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 19:21:47 +0000

    Added solution for exercise 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- This script lists all records of the table second_table
+-- This query lists all records of the table second_table
+SELECT score, name FROM first_table WHERE `name` != "NULL" ORDER BY score DESC;
