30e040b397974fa19a553dd96fe67487a467cad2
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 21:03:28 +0000

    Updated statement to alter database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- This script converts the field name in the first_table from hbtn_0c_0 database to UTF8
 -- This query converts the field name in the first_table from hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
