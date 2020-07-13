de1063b7d7b18459a66e25471c46053d060a1202
OctopusHugz <colsonscott53@gmail.com>
2020-07-13 19:23:02 +0000

    Updated table name to second_table

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of the table second_table
 -- This query lists all records of the table second_table
-SELECT score, name FROM first_table WHERE `name` != "NULL" ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE `name` != "NULL" ORDER BY score DESC;
