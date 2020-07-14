cd93fdd45903e9187eab7b0872b3558a1d5887b4
OctopusHugz <colsonscott53@gmail.com>
2020-07-14 18:48:25 +0000

    Added second comment on exercise 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
+-- This project goes deeper into databases with user and permissions and joins
 -- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
