b87cd98875838de5884fde2434b535b5b649933b
OctopusHugz <colsonscott53@gmail.com>
2020-07-14 23:29:02 +0000

    Updated formatting

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@ SELECT ts.title,
 	tsg.genre_id
 FROM tv_shows AS ts
 	LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id
-	WHERE tsg.show_id IS NULL
+WHERE tsg.show_id IS NULL
 ORDER BY ts.title ASC,
 	tsg.genre_id ASC;
