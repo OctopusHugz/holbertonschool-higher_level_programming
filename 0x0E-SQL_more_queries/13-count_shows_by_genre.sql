211822fa439a95b9c00a4af4275fec3f2352a5a9
OctopusHugz <colsonscott53@gmail.com>
2020-07-15 02:57:59 +0000

    Added conditional logic for null

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,6 @@ SELECT tg.name genre,
 	COUNT(*) AS number_of_shows
 FROM tv_genres AS tg
 	LEFT JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
+WHERE tsg.genre_id IS NOT NULL
 GROUP BY tg.id
 ORDER BY number_of_shows DESC, tg.id ASC;
