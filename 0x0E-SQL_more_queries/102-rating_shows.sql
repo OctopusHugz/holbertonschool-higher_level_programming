cd113be0d23e550667fa95089faf83eba939c0e4
OctopusHugz <colsonscott53@gmail.com>
2020-07-15 19:35:41 +0000

    Added solution for exercise 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,3 +1,6 @@
 -- This script lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT ts.title
-FROM tv_shows AS ts;
+SELECT DISTINCT ts.title, SUM(tsr.rate) rating
+FROM tv_shows AS ts
+LEFT JOIN tv_show_ratings AS tsr ON ts.id = tsr.show_id
+GROUP BY ts.title
+ORDER BY rating DESC;
