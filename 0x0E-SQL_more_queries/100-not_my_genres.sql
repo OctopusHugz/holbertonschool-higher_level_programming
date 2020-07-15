5eaf2ce1380e2ba928e35b53910b59d2dd52a1c5
OctopusHugz <colsonscott53@gmail.com>
2020-07-15 04:54:52 +0000

    Removed comments

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,14 +5,3 @@ FROM tv_genres AS tg
 	LEFT JOIN tv_shows AS ts ON tsg.show_id = ts.id
 WHERE ts.title = "Dexter"
 ORDER BY tg.name;
-
-/* SELECT tg.name
-FROM tv_genres AS tg
-	LEFT JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
-	LEFT JOIN tv_shows AS ts ON tsg.show_id = ts.id
-WHERE ts.title = "Dexter"
-ORDER BY tg.name; */
-/* WHERE id_key NOT IN (
-		select id_key
-		FROM id_key_table
-	); */
