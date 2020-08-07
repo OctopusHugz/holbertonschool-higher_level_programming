fe1ee834683dde1d59673a44d3ee93241ad5b273
OctopusHugz <colsonscott53@gmail.com>
2020-08-07 18:52:37 +0000

    Added docstrings and put code into a function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""This module lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+
+def get_all_states():
+    """This function gets all the states and prints them row by row"""
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
@@ -11,3 +14,6 @@ if __name__ == '__main__':
         print(row)
     cur.close()
     db.close()
+
+if __name__ == '__main__':
+    get_all_states()
