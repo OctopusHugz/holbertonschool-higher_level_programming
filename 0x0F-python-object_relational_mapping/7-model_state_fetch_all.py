a983c39ce8f6cb975be3ea09b52c8a9bcb197d62
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 18:08:03 +0000

    Fixed import issues and added create_engine and sessionmaker

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module lists all State objects from the database hbtn_0e_6_usa"""
-from model_state import Base, State, Session
+from sys import argv
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from model_state import Base, State
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
 
 
 def model_state_fetch_all():
