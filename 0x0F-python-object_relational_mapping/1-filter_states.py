5840461289e83c5bf9ebb9a920757e82dd2d600f
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 17:11:02 +0000

    Added exception printing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,14 +11,14 @@ and prints them row by row"""
     try:
         db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
         rows = cur.fetchall()
         for row in rows:
             print(row)
         cur.close()
         db.close()
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     filter_states()
