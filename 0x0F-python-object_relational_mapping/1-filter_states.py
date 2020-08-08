7cb8afd8069c74cf0b3c1b49e37a1da7fd86f43e
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 17:26:26 +0000

    Removed try/except

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,19 +8,15 @@ from sys import argv
 def filter_states():
     """This function gets all the states starting with the letter N
 and prints them row by row"""
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=argv[1], passwd=argv[2], db=argv[3])
-        cur = db.cursor()
-        cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
-                    ORDER BY id ASC;""")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
-    except Exception as e:
-        print(e)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
 
 if __name__ == '__main__':
