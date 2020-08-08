c4fc8d5bcab68c86d179e52abf716e8fc0419338
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 17:13:22 +0000

    Added host and port to connect statement

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ def filter_states():
     """This function gets all the states starting with the letter N
 and prints them row by row"""
     try:
-        db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=argv[1], passwd=argv[2], db=argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
         rows = cur.fetchall()
@@ -19,6 +20,7 @@ and prints them row by row"""
         db.close()
     except Exception as e:
         print(e)
+
 
 if __name__ == '__main__':
     filter_states()
