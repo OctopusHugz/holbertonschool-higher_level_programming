1f2a06f4987b685fec69cea26c271272544331ef
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 20:36:33 +0000

    Checking to see if ORDER BY is needed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,7 @@ from sys import argv
 def my_filter_states():
     """This function gets all the states matching the expression provided by
 the user and prints them row by row"""
-    string = """SELECT * FROM states WHERE
-    name = '{}';""".format(argv[4])
+    string = "SELECT * FROM states WHERE name = '{}';".format(argv[4])
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(string)
