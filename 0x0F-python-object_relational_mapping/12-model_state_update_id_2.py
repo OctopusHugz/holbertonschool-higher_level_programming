90c925108fb30714bb4a663ee2930c3538a96d68
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 18:38:30 +0000

    Added create_engine and sessionmaker to main block

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """This module lists the first State object from the database hbtn_0e_6_usa"""
-from model_state import Session, State
+from sys import argv
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from model_state import Base, State
 
 
 def model_state_update_id_2():
     """This function lists 1st State object from the database hbtn_0e_6_usa"""
-    try:
-        session = Session()
-        state = session.query(State).filter(State.id == 2)
-        print(state[0].name)
-        state[0].name = "New Mexico"
-        print(state[0].name)
-        session.commit()
-        #session.close()
-    except Exception as e:
-        print(e)
+    session = Session()
+    state = session.query(State).filter(State.id == 2)
+    print(state[0].name)
+    state[0].name = "New Mexico"
+    print(state[0].name)
+    session.commit()
+    session.close()
 
 
 if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     model_state_update_id_2()
