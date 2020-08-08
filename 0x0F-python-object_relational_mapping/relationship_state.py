2403feaab20a3249b2e88b584971e07b68505975
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 23:01:02 +0000

    Added relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module creates a State class"""
+from sqlalchemy.orm import backref, relationship
+from model_city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """This is an instance of the State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
