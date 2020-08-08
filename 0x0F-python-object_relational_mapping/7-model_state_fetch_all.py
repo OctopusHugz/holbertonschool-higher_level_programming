9a9bb2ebb9b00fb94c1f1b7be30bd6363c1b41ee
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 03:03:35 +0000

    Removed unused imports and commented out code

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 """This module lists all State objects from the database hbtn_0e_6_usa"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (create_engine)
-from sqlalchemy import Table
-from sqlalchemy import select
-from sys import argv
-
-from sqlalchemy.sql.schema import MetaData
-from model_state import Base, State, Session
-
-Base = declarative_base()
+from model_state import Session, State
 
 
 def model_state_fetch_all():
@@ -20,14 +10,6 @@ def model_state_fetch_all():
         states = session.query(State).all()
         for state in states:
             print(str(state.id) + ": " + str(state.name))
-            #print(state.id, state.name)
-        #conn = engine.connect()
-        #meta = MetaData(engine)
-        #table = meta.tables['states']
-        #ss = select([table])
-        #rows = conn.execute(ss)
-        #for row in rows:
-        #    print(row)
     except:
         pass
 
