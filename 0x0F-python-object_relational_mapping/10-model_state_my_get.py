d3593dffa8655e69a20d30e0553eb4a15e94c90d
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 18:30:17 +0000

    Added create_engine and sessionmaker to main block

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """This module lists the first State object from the database hbtn_0e_6_usa"""
 from sys import argv
-from model_state import Session, State
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from model_state import Base, State
 
 
 def model_state_my_get():
     """This function lists 1st State object from the database hbtn_0e_6_usa"""
-    try:
-        session = Session()
-        states = session.query(State).all()
-        for state in states:
-            if argv[4] == state.name:
-                print(state.id)
-                return
-        print("Not found")
-    except:
-        pass
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        if argv[4] == state.name:
+            print(state.id)
+            return
+    print("Not found")
 
 
 if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     model_state_my_get()
