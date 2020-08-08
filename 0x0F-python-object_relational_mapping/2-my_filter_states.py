3eff9bb5fea6811879418022ed818194d9abe72c
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 20:45:39 +0000

    Added LIKE BINARY and removed check in for loop

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,15 @@ from sys import argv
 def my_filter_states():
     """This function gets all the states matching the expression provided by
 the user and prints them row by row"""
-    string = "SELECT * FROM states WHERE name = '{}';".format(argv[4])
+    string = """SELECT * FROM states WHERE name
+    LIKE BINARY '{}';""".format(argv[4])
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(string)
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+        # if row[1] == argv[4]:
+        print(row)
     cur.close()
     db.close()
 
