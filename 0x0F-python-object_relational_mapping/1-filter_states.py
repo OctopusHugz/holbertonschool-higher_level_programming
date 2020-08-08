ff6eaa9f0c3baf42b2a47307a66e83ea6df99fe2
OctopusHugz <colsonscott53@gmail.com>
2020-08-08 17:31:39 +0000

    Moved imports to function. Checked for rows existence before iterating

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,18 +6,19 @@ database hbtn_0e_0_usa starting with N"""
 def filter_states():
     """This function gets all the states starting with the letter N
 and prints them row by row"""
+    import MySQLdb
+    from sys import argv
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if rows:
+        for row in rows:
+            print(row)
     cur.close()
     db.close()
 
 
 if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
     filter_states()
