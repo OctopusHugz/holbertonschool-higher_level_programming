1498b7d90291ffbb224dcdb9b752d36d4bd1f7ee
OctopusHugz <colsonscott53@gmail.com>
2020-08-10 16:17:06 +0000

    Added import MYSQLdb to file unnecessarily

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module lists all states from the database hbtn_0e_0_usa"""
 from base import cur, db
-
+import MySQLdb
 
 def select_states():
     """This function gets all the states and prints them row by row"""
