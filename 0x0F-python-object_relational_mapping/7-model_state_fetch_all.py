14bdc63dad7b7b67f99987a454e6b0dd7063163c
OctopusHugz <colsonscott53@gmail.com>
2020-08-10 16:33:29 +0000

    Rollback

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module lists all State objects from the database hbtn_0e_6_usa"""
-from model_state import Base, State, engine, Session
-import sqlalchemy
+from sys import argv
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from model_state import Base, State
 
 
 def model_state_fetch_all():
@@ -11,5 +13,8 @@ def model_state_fetch_all():
         print(state)
 
 if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     model_state_fetch_all()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 """This module creates a State class"""
-from sys import argv
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.session import sessionmaker
 
 
 Base = declarative_base()
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True, echo=True)
-Session = sessionmaker(bind=engine)
 
 
 class State(Base):
