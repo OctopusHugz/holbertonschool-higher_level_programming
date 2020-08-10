60a7b4de2de8f36e739dc3d8a698d2d7fc6e83d9
OctopusHugz <colsonscott53@gmail.com>
2020-08-10 16:31:19 +0000

    Added import sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module lists all State objects from the database hbtn_0e_6_usa"""
 from model_state import Base, State, engine, Session
+import sqlalchemy
 
 
 def model_state_fetch_all():
