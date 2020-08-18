a5691ea9279b6734b19a5a8996c79c19398a6386
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 18:44:53 +0000

    Added quotes to prevent globbing

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl $1 -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
 
