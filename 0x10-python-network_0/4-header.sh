0343c9469e0fc725376f21fecc17a5703ef1b1de
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 18:41:34 +0000

    Added solution for exercise 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl $1 -s -H "X-HolbertonSchool-User-Id: 98"
+curl $1 -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
