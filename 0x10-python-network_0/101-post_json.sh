f7f48e637f760ae3993109e025a49a4309786ddd
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 19:19:11 +0000

    Added @ in front of filename

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl $1 -sX POST -H "Content-Type: application/json" -d $2
+curl $1 -sX POST -H "Content-Type: application/json" -d @$2
