01dad69858a8721b421129a9b98171b7dd6e9b35
OctopusHugz <colsonscott53@gmail.com>
2020-08-18 21:22:17 +0000

    Fixed pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module contains a peak finding function"""
+
+
 def find_peak(list_of_integers):
     """This function finds a peak in a list of unsorted integers"""
     if list_of_integers:
