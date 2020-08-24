e32e0b23d8f6fb71d700754318f7aa525db40b6f
OctopusHugz <colsonscott53@gmail.com>
2020-08-24 17:44:34 +0000

    Added initial outline for exercise 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module creates a python script to interact with a url"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    rj = {}
+    try:
+        q = argv[1]
+    except:
+        q = ""
+    response = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        rj = response.json()
+    except:
+        print("Not a valid JSON")
+    if len(rj) == 0:
+        print("No result")
+    else:
+        print("[{:d}] {}".format(rj.get("id"), rj.get("name")))
