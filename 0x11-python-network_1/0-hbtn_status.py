0354624277eae92cd7fad5d8db3ef0282ac6c9db
OctopusHugz <colsonscott53@gmail.com>
2020-08-24 16:29:01 +0000

    Fixed html decoding

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(html.__class__))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(response.msg))
+        print("\t- utf8 content: {}".format(html.decode()))
