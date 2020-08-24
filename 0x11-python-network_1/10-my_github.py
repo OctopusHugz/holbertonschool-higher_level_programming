7bd5621a386dcb90bb6a4de8222da473a545f0f4
OctopusHugz <colsonscott53@gmail.com>
2020-08-24 18:34:00 +0000

    Added solution for exercise 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module creates a python script to interact with a url"""
+
+from requests.models import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    response = requests.get("https://api.github.com/user", auth=HTTPBasicAuth(
+        argv[1], argv[2]))
+    print(response.json().get("id"))
