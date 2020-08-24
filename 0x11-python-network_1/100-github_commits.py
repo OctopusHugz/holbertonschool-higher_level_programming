09b8d7fd471c12ee5d6f0634b0e3b4d2fe989a04
OctopusHugz <colsonscott53@gmail.com>
2020-08-24 18:58:37 +0000

    Added initial solution for exercise 10. I see you Daniel Colson!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module creates a python script to interact with a url"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    repo = argv[1]
+    owner = argv[2]
+    response = requests.get(
+        "https://api.github.com/repos/{}/{}/commits".format(owner, repo))
+    for commit in response.json()[:10]:
+        print("{}: {}".format(commit.get("sha"),
+                        commit["commit"]["author"].get("name")))
