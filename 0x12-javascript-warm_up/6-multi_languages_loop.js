21366c1c3cff147f47d10aad6753bfdcd30f9c22
OctopusHugz <colsonscott53@gmail.com>
2020-09-15 21:51:48 +0000

    Removed forEach and used for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-langs.forEach((lang) => console.log(lang));
+for (let i = 0; langs[i]; i++) {
+  console.log(langs[i]);
+}
