353fe2d89effd273efe3795ada8d3b3cc554b3e6
OctopusHugz <colsonscott53@gmail.com>
2020-09-16 04:53:11 +0000

    Trying new method for 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,16 @@ if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
   const newArr = process.argv.slice(2, length);
-  newArr.sort();
-  if (parseInt(newArr[newArr.length - 1], 10) > parseInt(newArr[0], 10)) {
-    newArr.reverse();
-  }
-  newArr.shift();
+  newArr.sort((a, b) => a - b);
+  newArr.pop();
   console.log(Math.max(...newArr));
 }
+// } else {
+//   const newArr = process.argv.slice(2, length);
+//   newArr.sort();
+//   if (parseInt(newArr[newArr.length - 1], 10) > parseInt(newArr[0], 10)) {
+//     newArr.reverse();
+//   }
+//   newArr.shift();
+//   console.log(Math.max(...newArr));
+// }
