7dd62c800095c454dfc8279eba79bc6e1aa77e16
OctopusHugz <colsonscott53@gmail.com>
2020-09-16 04:54:27 +0000

    Removed extraneous comments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
-const length = process.argv.length;
 if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
-  const newArr = process.argv.slice(2, length);
+  const newArr = process.argv.slice(2, process.argv.length);
   newArr.sort((a, b) => a - b);
   newArr.pop();
   console.log(Math.max(...newArr));
 }
-// } else {
-//   const newArr = process.argv.slice(2, length);
-//   newArr.sort();
-//   if (parseInt(newArr[newArr.length - 1], 10) > parseInt(newArr[0], 10)) {
-//     newArr.reverse();
-//   }
-//   newArr.shift();
-//   console.log(Math.max(...newArr));
-// }
