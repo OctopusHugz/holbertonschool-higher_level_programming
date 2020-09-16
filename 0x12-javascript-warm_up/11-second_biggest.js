54778b151c0cc2bb726ded6d36eb88348af9f4b2
OctopusHugz <colsonscott53@gmail.com>
2020-09-16 03:41:38 +0000

    Updated solution for 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
+const length = process.argv.length;
 if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
 } else {
-  const newArr = process.argv.slice(2, process.argv.length);
-  newArr.sort().reverse().shift();
+  const newArr = process.argv.slice(2, length);
+  newArr.sort();
+  if (parseInt(newArr[newArr.length - 1], 10) > parseInt(newArr[0], 10)) {
+    newArr.reverse();
+  }
+  newArr.shift();
   console.log(Math.max(...newArr));
 }
