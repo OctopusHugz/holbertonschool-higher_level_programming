4f6c5a00b85950062cf101d1b5ff19f7e483be10
OctopusHugz <colsonscott53@gmail.com>
2020-09-16 03:17:12 +0000

    Added else to fix control flow

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
+} else {
+  const newArr = process.argv.slice(2, process.argv.length);
+  newArr.sort().reverse().shift();
+  console.log(Math.max(...newArr));
 }
-const newArr = process.argv.slice(2, process.argv.length);
-newArr.sort().reverse().shift();
-console.log(Math.max(...newArr));
