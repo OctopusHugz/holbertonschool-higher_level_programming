c03b9427bb488f7cf3887267b1f03aec919ee484
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:47:50 +0000

    Try removing function from map and run with params

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 if (list) {
-  const array2 = list.map(function (num, index) {
-    if (!isNaN(num)) {
-      return parseInt(num * index);
-    }
-  });
+  const array2 = list.map((num, index) => num * index);
   console.log(list);
   console.log(array2);
 }
