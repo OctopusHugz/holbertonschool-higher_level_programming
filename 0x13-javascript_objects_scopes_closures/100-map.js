f9745a990b712c83d77576604381d2632a20f7ae
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:42:44 +0000

    Check for if list first

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const array2 = list.map(function (num, index) {
-  if (!isNaN(num)) {
-    return parseInt(num * index);
-  }
-});
-console.log(list);
-console.log(array2);
+if (list) {
+  const array2 = list.map(function (num, index) {
+    if (!isNaN(num)) {
+      return parseInt(num * index);
+    }
+  });
+  console.log(list);
+  console.log(array2);
+}
