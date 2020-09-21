c87931a2150acd179767cb6018b7c498ec4de1fc
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 21:02:06 +0000

    Added solution for exercise 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,22 @@
 #!/usr/bin/node
-const concat = require('concat-files');
-concat([process.argv[2], process.argv[3]], process.argv[4], function (err) {
-  if (err) throw err;
-  console.log('done');
+const fs = require('fs');
+const read1 = fs.readFile(process.argv[2], function (err, fd) {
+  if (err) {
+    return console.error(err);
+  }
+  fs.writeFile(process.argv[4], fd.toString(), { flag: 'a' }, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
 });
-
-// const fs = require('fs');
-// const file1 = fs.open(process.argv[2], 'r', function (err, fd) {
-//   if (err) {
-//     return console.error(err);
-//   }
-// });
-// const file2 = fs.readFile(process.argv[3]);
-// console.log(file1);
+const read2 = fs.readFile(process.argv[3], function (err, fd) {
+  if (err) {
+    return console.error(err);
+  }
+  fs.writeFile(process.argv[4], fd.toString(), { flag: 'a' }, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+});
