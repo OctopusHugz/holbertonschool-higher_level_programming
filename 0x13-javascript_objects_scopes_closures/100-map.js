a24faaaf243ac0abf18965afc6baa3c2dd2167ca
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:40:42 +0000

    Added check for \!isNan(num) so that strings will return undefined

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-// const array2 = list.map((x) => x * list.indexOf(x));
 const array2 = list.map(function (num, index) {
-  return parseInt(num * index);
+  if (!isNaN(num)) {
+    return parseInt(num * index);
+  }
 });
 console.log(list);
 console.log(array2);
-// array2.forEach(currentItem => {
-//   if (Object.is(array2[currentItem], -0)) {
-//     array2[currentItem] = 0;
-//   }
-// });
-// console.log(array2);
