fb8e13b4ffdaf52f47a3c0cce27ba2f8cf69ab6b
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:12:42 +0000

    Added check for list.length > 0

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const array2 = list.map((x) => x * list.indexOf(x));
-console.log(list);
-console.log(array2);
+if (list.length > 0) {
+  const array2 = list.map((x) => x * list.indexOf(x));
+  console.log(list);
+  console.log(array2);
+}
