46daeb20d85aa1852586e41f1669a7280e7f7255
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:54:04 +0000

    Revert back to arrow func and test whether if list check is necessary

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-if (list) {
-  const array2 = list.map(function (num, index) {
-    return num * index;
-  });
-  console.log(list);
-  console.log(array2);
-}
+const list2 = list.map((num, index) => num * index);
+console.log(list);
+console.log(list2);
