32e31bc77fd096801daf4480e5642337b975db71
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 19:33:45 +0000

    Changed var to const

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const newDict = {};
-for (var key in dict) {
-  var value = dict[key];
+for (const key in dict) {
+  const value = dict[key];
   if (newDict[value]) newDict[value].push(key);
   else {
     newDict[value] = [];
