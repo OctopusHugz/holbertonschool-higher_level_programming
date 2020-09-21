a269e55c915773262ca3bd43ef507607d9ae0745
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:49:07 +0000

    Just for fun seeing if function works without parseInt in return

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 if (list) {
-  const array2 = list.map((num, index) => num * index);
+  const array2 = list.map(function (num, index) {
+    return num * index;
+  });
   console.log(list);
   console.log(array2);
 }
