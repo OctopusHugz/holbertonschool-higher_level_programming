ab1ce8c31d55647244a9f8dca23799335bacf044
OctopusHugz <colsonscott53@gmail.com>
2020-09-21 18:32:33 +0000

    Added function to map and parseInt the return value

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-if (list.length > 0) {
-  const array2 = list.map((x) => x * list.indexOf(x));
-  console.log(list);
-  console.log(array2);
-}
+// const array2 = list.map((x) => x * list.indexOf(x));
+const array2 = list.map(function (num, index) {
+  return parseInt(num * index);
+});
+console.log(list);
+console.log(array2);
+// array2.forEach(currentItem => {
+//   if (Object.is(array2[currentItem], -0)) {
+//     array2[currentItem] = 0;
+//   }
+// });
+// console.log(array2);
