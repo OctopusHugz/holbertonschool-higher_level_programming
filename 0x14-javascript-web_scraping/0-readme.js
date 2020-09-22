70531d2a40e9e1ea93dfca4c1a60d80588b8ac30
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 16:57:39 +0000

    Changed console.error to console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], { encoding: 'utf-8' }, function (err, fd) {
   if (err) {
-    return console.error(err);
+    return console.log(err);
   }
   console.log(fd.toString());
 });
