b83f3b6ec59fa96b777f567e3fbcb071b909d3f3
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 18:51:36 +0000

    Removed else

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,14 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) console.log(error);
-  else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes('18')) {
-          count++;
-        }
-      });
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((film) => {
+    film.characters.forEach((character) => {
+      if (character.includes('18')) {
+        count++;
+      }
     });
-    console.log(count);
-  }
+  });
+  console.log(count);
 });
