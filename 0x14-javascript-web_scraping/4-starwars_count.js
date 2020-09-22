01f48e782f8e73fb0835b514b9c5bb3de0fd2ead
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 18:17:04 +0000

    Changed to return console.log(error)

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-          count++;
-        }
-      });
+  if (error) return console.log(error);
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((film) => {
+    film.characters.forEach((character) => {
+      if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count++;
+      }
     });
-    console.log(count);
-  }
+  });
+  console.log(count);
 });
