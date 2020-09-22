4ed2eb11aa30b2ac2d910b2581722f955e5309cf
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 16:08:12 +0000

    Added solution for exercise 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) console.error('error:', error);
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((currentItem) => {
+    currentItem.characters.forEach((currentItem) => {
+      if (currentItem === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count++;
+      }
+    });
+  });
+  console.log(count);
+});
