83e4181c1f855c2dea69c7b09755c66760f056a2
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 16:23:13 +0000

    Added solution for exercise 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const newDict = {};
+request(process.argv[2], function (error, response, body) {
+  if (error) console.error('error:', error);
+  const result = JSON.parse(body);
+  result.forEach((currentItem) => {
+    if (!newDict[currentItem.userId]) {
+      newDict[currentItem.userId] = 0;
+    }
+    if (currentItem.completed === true) {
+      newDict[currentItem.userId]++;
+    }
+  });
+  console.log(newDict);
+});
