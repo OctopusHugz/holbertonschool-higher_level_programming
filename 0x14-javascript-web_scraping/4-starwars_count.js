510870dbea345271f69a686c28daae867ae6f9eb
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 17:40:37 +0000

    Added try/catch block

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,16 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) console.log(error);
-  const films = JSON.parse(body).results;
-  let count = 0;
-  films.forEach((currentItem) => {
-    currentItem.characters.forEach((currentItem) => {
-      if (currentItem === 'https://swapi-api.hbtn.io/api/people/18/') {
-        count++;
-      }
+  try {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach((currentItem) => {
+      currentItem.characters.forEach((currentItem) => {
+        if (currentItem === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      });
     });
-  });
-  console.log(count);
+    console.log(count);
+  } catch (error) {}
 });
