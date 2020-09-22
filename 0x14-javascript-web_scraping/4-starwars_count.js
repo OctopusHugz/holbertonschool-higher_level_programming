b4bfb256312208b97956006a28c05803eeea3fe7
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 18:27:44 +0000

    Added else block

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error) return console.log(error);
-  const films = JSON.parse(body).results;
-  let count = 0;
-  films.forEach((film) => {
-    film.characters.forEach((character) => {
-      if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
-        count++;
-      }
+  if (error) console.log(error);
+  else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      });
     });
-  });
-  console.log(count);
+    console.log(count);
+  }
 });
