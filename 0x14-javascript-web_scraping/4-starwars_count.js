f0caff9b730e9a2ea993a73ff785f41b02b1d6a6
OctopusHugz <colsonscott53@gmail.com>
2020-09-22 17:59:58 +0000

    Added check for not error and statusCode === 200

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error) console.log(error);
-  try {
+  if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
     let count = 0;
-    films.forEach((currentItem) => {
-      currentItem.characters.forEach((currentItem) => {
-        if (currentItem === 'https://swapi-api.hbtn.io/api/people/18/') {
+    films.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
           count++;
         }
       });
     });
     console.log(count);
-  } catch (error) {}
+  }
 });
