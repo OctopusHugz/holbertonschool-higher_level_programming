ecae682452aedc13ea791bcfbf6f2ac380beaaa4
OctopusHugz <colsonscott53@gmail.com>
2020-09-24 18:11:03 +0000

    Changed INPUT to input for consistency

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const $ = window.$;
 $(document).ready(function () {
-  $('INPUT#btn_translate').click(function () {
-    const language = $('INPUT#language_code').val();
+  $('input#btn_translate').click(function () {
+    const language = $('input#language_code').val();
     const url = 'https://fourtonfish.com/hellosalut/?lang=' + language;
     $.get(url, function (data) {
       $('div#hello').html(data.hello);
